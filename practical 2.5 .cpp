e79bb2fa5d2b4892b1e704fa1b57c6c31be451b1
SimranBambani <simran.bambani510@gmail.com>
2025-04-17 01:26:21 -0700

    practical 2.5 .cpp

--- practical 2.5 .cpp
+++ practical 2.5 .cpp
@@ -9,27 +9,27 @@ class Loan_Master
     int loan_tenure;
     static float interest_rate;
 public:
-    void get_data(string name,float amount,int tenure)
+    void Get_Data(string name,float amount,int tenure)
     {
        applicants_name=name;
        loan_amount=amount;
        loan_tenure=tenure;
-       cout<<"EMI= "<<EMI_calculator(loan_amount,loan_tenure)<<endl;
+       cout<<"EMI= "<<EMI_Calculator(loan_amount,loan_tenure)<<endl;
 
     }
-     double EMI_calculator(float amount,int tenure)
+     double EMI_Calculator(float amount,int tenure)
     {
         EMI=(loan_amount*interest_rate*pow(1+interest_rate,loan_tenure))/(pow(1+interest_rate,loan_tenure)-1);
         return EMI;
     }
-    void display()
+    void Display()
     {
         cout<<"---------------------------------------"<<endl;
         cout<<"Name of the applicant is : "<<applicants_name<<endl;
         cout<<"Total amount of the loan is: "<<loan_amount<<endl;
         cout<<"Interest rate is: "<<interest_rate<<endl;
         cout<<"Loan tenure is: "<<loan_tenure<<endl;
-        cout<<"EMI= "<<EMI_calculator(loan_amount,loan_tenure)<<endl;
+        cout<<"EMI= "<<EMI_Calculator(loan_amount,loan_tenure)<<endl;
 
     }
 }loan[100];
@@ -42,32 +42,28 @@ int main()
    char input;
    do
    {
-       cout<<"Enter'C' if you want to create a account or Enter 'D' to create a account or Enter 'E' to exit: ";
+       cout<<"Enter'C' if you want to create a account or Enter 'D' to display a account or Enter 'E' to exit: ";
        cin>>input;
        switch(input)
        {
        case'C':
-   cout<<"Enter the name of the applicant: ";
-   fflush(stdin);
-   getline(cin,name);
-   cout<<"Enter the loan amount: ";
-   cin>>amount;
-   cout<<"Enter the loan tenure: ";
-   cin>>tenure;
-
-   loan[count].get_data(name,amount,tenure);
-   count++;
-   break;
+            cout<<"Enter the name of the applicant: ";
+            fflush(stdin);
+            getline(cin,name);
+            cout<<"Enter the loan amount: ";
+            cin>>amount;
+            cout<<"Enter the loan tenure: ";
+            cin>>tenure;
+            loan[count].Get_Data(name,amount,tenure);
+            count++;
+            break;
     case 'D':
            for(i=0;i<count;i++)
            {
-        loan[i].display();
+              loan[i].Display();
            }
            break;
-       default:
-        cout<<"Enter a valid function to perform."<<endl;
-        break;
        }
    }while(input!='E');
-
+    return 0;
 }
