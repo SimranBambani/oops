517692a4de99dacd0d683445c9ee64071a751924
SimranBambani <simran.bambani510@gmail.com>
2025-04-17 01:10:09 -0700

    pracitcal 3.1.cpp

--- pracitcal 3.1.cpp
+++ pracitcal 3.1.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 const double DEFAULT_BONUS = 5000.0;
-class employee_salary
+class Employee_Salary
 {
     float basic_salary;
     float employee_bonus,total_salary;
     string employee_name;
 public:
-    employee_salary()
+    Employee_Salary()
     {
         char choice;
         fflush(stdin);
-       cout<<"Enter the name of employee: ";
-       cin>>ws;
+        cout<<"Enter the name of employee: ";
+        cin>>ws;
         getline(cin,employee_name);
         fflush(stdin);
         cout<<"Enter the salary of the employee: ";
@@ -29,16 +29,16 @@ public:
            employee_bonus=DEFAULT_BONUS;
         }
     }
-    inline void salary()
+    inline void Salary_of_Employee()
     {
         total_salary=basic_salary+employee_bonus;
     }
-    void display()
+    void Display()
     {
         cout<<"Name of the employee: "<< employee_name<<endl;
         cout<<"Salary of the employee: "<<basic_salary<<endl;
         cout<<"Bonus amount of the employee: "<<employee_bonus<<endl;
-          cout<<"Total salary of the employee: "<<total_salary<<endl;
+        cout<<"Total salary of the employee: "<<total_salary<<endl;
     }
 };
 int main()
@@ -49,12 +49,12 @@ int main()
     fflush(stdin);
     float salary,bonus;
     string name;
-    employee_salary *ptr=new employee_salary[num];
+    Employee_Salary *ptr=new Employee_Salary[num];
     for(i=0;i<num;i++)
     {
 
-        ptr[i].salary();
-        ptr[i].display();
+        ptr[i].Salary_of_Employee();
+        ptr[i].Display();
 
     }
     return 0;
