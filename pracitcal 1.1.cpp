4c00d82ce2bb71722dfa5dd22e324404e7abb31b
SimranBambani <simran.bambani510@gmail.com>
2025-04-17 01:00:04 -0700

    practical 1.1.cpp

--- pracitcal 1.1.cpp
+++ pracitcal 1.1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-class BackAccount
+class Bank_Account
 {
 private:
     string acc_holder_name;
@@ -8,21 +8,21 @@ private:
     float widr_amount;
     double acc_number;
     float balance;
-   public:
-    void display(){
+public:
+    void Get_Data(){
         cout<<"Enter the account holder's name: ";
         getline(cin,acc_holder_name);
         cout<<"Enter the account number: ";
         cin>>acc_number;
     }
-    float deposit()
+    float Deposit_Amount()
     {
         cout<<"Enter amount deposited: ";
         cin>>depo_amount;
         cout<<"AMOUNT DEPOSITED: "<<depo_amount<<endl;
         return 0;
     }
-    float withdraw()
+    float Withdraw_Amount()
     {
         cout<<"Enter amount to be withdraw: ";
         cin>>widr_amount;
@@ -33,7 +33,7 @@ private:
         }
         return 0;
     }
-    float balance_amount()
+    float Balance_Amount()
     {
         balance=depo_amount-widr_amount;
         if(balance>0){
@@ -41,14 +41,24 @@ private:
         }
         return 0;
     }
+    void Display()
+    {
+        cout<<"Account Holder Name: "<<acc_holder_name<<endl;
+        cout<<"Account Number: "<<acc_number<<endl;
+        cout<<"Deposited Amount: "<<depo_amount<<endl;
+        cout<<"Withdraw Amount: "<<widr_amount<<endl;
+        cout<<"Balance Amount: "<<balance<<endl;
+    }
 
-}s1;
+};
 int main()
 {
-    s1.display();
-    s1.deposit();
-    s1.withdraw();
-    s1.balance_amount();
+    Bank_Account s1;
+    s1.Get_Data();
+    s1.Deposit_Amount();
+    s1.Withdraw_Amount();
+    s1.Balance_Amount();
+    s1.Display();
     cout<<"--------------------------------"<<endl;
     cout<<"NAME: SIMRAN BAMBANI"<<endl;
     cout<<"ID NO: 24CE005";
