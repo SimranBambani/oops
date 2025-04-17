51d8ba567df217a76f43df7290288177093c2619
SimranBambani <simran.bambani510@gmail.com>
2025-04-17 01:27:43 -0700

    practical 3.3.cpp

--- practical 3.3.cpp
+++ practical 3.3.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
 using namespace std;
-
-class Account_details {
+class Account_details
+{
     string holder_name;
     int acc_num;
     float acc_balance;
     float total_amount;
-
 public:
-    void add_account(string n, int num, float bal) {
+    void Add_Account(string n, int num, float bal) {
         holder_name = n;
         acc_num = num;
         acc_balance = bal;
         total_amount = bal;
     }
 
-    int get_account_number() {
+    int Get_Account_Number() {
         return acc_num;
     }
 
-    int search_account(int s) {
+    int Search_Account(int s) {
         return s == acc_num ? 1 : 0;
     }
 
-    float total_amount_return() {
+    float Total_Amount_Return() {
         return total_amount;
     }
 
-    void deposit_amount(float d) {
+    void Deposit_Amount(float d) {
         total_amount += d;
     }
 
-    int withdraw_amount(float d) {
+    int Withdraw_Amount(float d) {
         if (total_amount >= d) {
             total_amount -= d;
             return 1;
@@ -39,7 +38,7 @@ public:
         return 0;
     }
 
-    void display_balance() {
+    void Display_Balance() {
         cout << "Account Number: " << acc_num << " has balance: " << total_amount << endl;
     }
 };
@@ -53,7 +52,6 @@ int main() {
         cout << "\n1 = Create Account\n2 = Transfer Money\n3 = Show Total Number of Accounts\n4 = Exit\n";
         cout << "Your choice: ";
         cin >> choice;
-
         switch (choice) {
             case 1: {
                 string name;
@@ -67,55 +65,48 @@ int main() {
                 cin >> a_num;
                 cout << "Enter initial balance: ";
                 cin >> a_bal;
-
                 Account_details* temp = new Account_details[acc_counter + 1];
-
                 for (int i = 0; i < acc_counter; i++) {
                     temp[i] = accounts[i];
                 }
-
                 accounts = temp;
-
-                accounts[acc_counter].add_account(name, a_num, a_bal);
+                accounts[acc_counter].Add_Account(name, a_num, a_bal);
                 acc_counter++;
-
                 cout << "Account created successfully!\n";
                 break;
             }
-
             case 2: {
                 int from_acc, to_acc;
                 float amount;
                 int from_found = 0, to_found = 0;
                 Account_details* from_account;
                 Account_details* to_account;
-
                 cout << "Enter account number FROM which money is to be transferred: ";
                 cin >> from_acc;
                 cout << "Enter account number TO which money is to be transferred: ";
                 cin >> to_acc;
                 cout << "Enter amount to transfer: ";
                 cin >> amount;
-
                 for (int i = 0; i < acc_counter; i++) {
-                    if (accounts[i].search_account(from_acc)) {
+                    if (accounts[i].Search_Account(from_acc)) {
                         from_found = 1;
                         from_account = &accounts[i];
                     }
-                    if (accounts[i].search_account(to_acc)) {
+                    if (accounts[i].Search_Account(to_acc)) {
                         to_found = 1;
                         to_account = &accounts[i];
                     }
                 }
-
-                if (!from_found || !to_found) {
-                    cout << "One or both account numbers not found!\n";
-                } else if (from_account->withdraw_amount(amount)) {
-                    to_account->deposit_amount(amount);
+                if (!from_found || !to_found)
+                    {
+                      cout << "One or both account numbers not found!\n";
+                } else if (from_account->Withdraw_Amount(amount)) {
+                    to_account->Deposit_Amount(amount);
                     cout << "Transaction successful!\n";
-                    from_account->display_balance();
-                    to_account->display_balance();
-                } else {
+                    from_account->Display_Balance();
+                    to_account->Display_Balance();
+                }
+                else {
                     cout << "Insufficient balance in source account!\n";
                 }
                 break;
@@ -133,7 +124,6 @@ int main() {
                 cout << "Invalid choice! Please enter a valid option."<<endl;
                 break;
         }
-
     } while (choice != 4);
     return 0;
 }
