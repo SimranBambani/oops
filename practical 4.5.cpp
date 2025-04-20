033cceba38d4ce9356c259d0f611893f86f03e0c
SimranBambani <simran.bambani510@gmail.com>
2025-04-19 23:09:36 -0700

    practical 4.5.cpp

--- practical 4.5.cpp
+++ practical 4.5.cpp
@@ -1,117 +1,91 @@
 #include<iostream>
 using namespace std;
-const int max_student=100;
-class Undergraduate;
-class Postgraduate;
-class Student {
-protected:
+const int max_student = 100;
+class Student_Grade
+{
+private:
     string name;
     int rollNo;
     double marks;
 public:
-    void inputData() {
+    void Get_Data() {
         cout<<"Enter student name: ";
-         cin>>name;
+        cin>>name;
         cout<<"Enter roll number: ";
-          cin>>rollNo;
-        cout<<"Enter marks : ";
+        cin>>rollNo;
+        cout<<"Enter marks: ";
         cin>>marks;
     }
-    virtual void compute_Grade() = 0;
-
-    virtual void display()
+    virtual void Calculate_Grade()
     {
-        cout<<"Name: " <<name<<endl;
+        cout<<"Base class grade calculation."<<endl;
+    }
+    void Display_Data() {
+        cout<<"Name: "<<name<<endl;
         cout<<"Roll No: "<<rollNo<<endl;
-        cout<<" Marks: "<< marks<< endl;
+        cout<<"Marks: "<<marks<<endl;
     }
-    virtual ~Student()
-    {}
+    ~Student_Grade() {}
 };
-class Undergraduate:public Student
- {
+class Undergraduate : public Student_Grade {
 public:
-    void compute_Grade()override
-    {
-        display();
+    void Calculate_Grade() {
+        Display_Data();
         cout<<"Undergraduate Grade: ";
-        if(marks>=85)
-            {
+        if(marks>= 85)
             cout<<"A"<<endl;
-            }
         else if(marks>=70)
-        {
             cout<<"B"<<endl;
-        }
-        else if (marks>=50)
-        {
+        else if (marks >= 50)
             cout<<"C"<<endl;
-        }
         else
-        {
             cout<<"F"<<endl;
-        }
     }
 };
-class Postgraduate:public Student
-{
+class Postgraduate : public Student_Grade {
 public:
-    void compute_Grade()override
-    {
-        display();
-        cout << "Postgraduate Grade: ";
-       if(marks>=90)
-            {
+    void Calculate_Grade()  {
+        Display_Data();
+        cout<<"Postgraduate Grade: ";
+        if(marks >= 90)
             cout<<"A"<<endl;
-            }
-        else if(marks>=75)
-        {
+        else if(marks>= 75)
             cout<<"B"<<endl;
-        }
-        else if (marks>=60)
-        {
+        else if(marks>=60)
             cout<<"C"<<endl;
-        }
         else
-        {
             cout<<"F"<<endl;
-        }
     }
 };
 int main() {
-    Student* students[max_student];
-    int n,choice,i;
-    cout<<"Enter number of students: ";
-    cin>> n;
-for(i = 0;i<n;++i)
-    {
-        cout<<"Enter 1 for Undergraduate, Enter 2 for Postgraduate: ";
-        cin>>choice;
-        if(choice == 1)
-         {
-            students[i]=new Undergraduate();
+    Student_Grade* students[max_student];
+    int number,choice;
+    int count = 0;
+    cout << "Enter number of students: ";
+    cin >> number;
+    for (int i = 0;i<number;i++) {
+        cout << "Enter 1 for Undergraduate, 2 for Postgraduate: ";
+        cin >> choice;
+        if (choice == 1) {
+            students[count]= new Undergraduate();
         }
-        else if (choice == 2)
-         {
-            students[i]=new Postgraduate();
+        else if (choice==2) {
+            students[count]=new Postgraduate();
         }
          else
-        {
-            cout << "Invalid choice. Skipping this student.\n";
-            students[i] = nullptr;
-            continue;
+         {
+            cout<< "Invalid choice. Please try again." << endl;
+            i--;
         }
-        students[i]->inputData();
+        students[count]->Get_Data();
+        count++;
     }
-    for(i=0;i<n;++i) {
-        if(students[i] != nullptr)
-            {
-            students[i]->compute_Grade();
-        }
+    for(int i=0;i<number;i++) {
+        students[i]->Calculate_Grade();
     }
-    for(i = 0; i < n; ++i) {
+
+    for(int i=0;i<count;i++) {
         delete students[i];
     }
     return 0;
 }
-
